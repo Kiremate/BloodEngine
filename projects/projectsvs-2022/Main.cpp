8589582451c4a0d3b63503e70f80252bdf974965
Erik <kiremate@hotmail.com>
2022-01-10 05:54:07 +0100

    Used Temp Angel's Code on Main.cpp Changed Readme's location

--- projects/projectsvs-2022/Main.cpp
+++ projects/projectsvs-2022/Main.cpp
@@ -1,19 +1,74 @@
-#include "SDL.h"
 
-int main(int argc, char* argv[])
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                                                             *
+ *  Este código es de dominio público.                                         *
+ *  angel.rodriguez@esne.edu                                                   *
+ *  2015.04                                                                    *
+ *                                                                             *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <SDL.h>
+#include <ciso646>
+
+int main(int, char**)
 {
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
+    // Se hace inicializa SDL:
 
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_Log("No se ha podido inicializar SDL2.");
+    }
+    else
+    {
+        // Se crea una ventana:
 
+        SDL_Window* window = SDL_CreateWindow("SDL2 Window Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+        if (not window)
+        {
+            SDL_Log("No se ha podido crear una ventana.");
+        }
+        else
+        {
+            // Se toma el buffer de la ventana para dibujar en él:
+
+            SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+            if (not surface)
+            {
+                SDL_Log("No se ha podido acceder al buffer de la ventana.");
+            }
+            else
+            {
+                bool exit = false;
+
+                do
+                {
+                    // Se procesan los eventos acumulados:
+
+                    SDL_Event event;
+
+                    while (SDL_PollEvent(&event) > 0)
+                    {
+                        if (event.type == SDL_QUIT)
+                        {
+                            exit = true;
+                        }
+                    }
+
+                    // Se rellena el fondo de la ventana de color gris:
+
+                    SDL_FillRect(surface, 0, SDL_MapRGB(surface->format, 225, 225, 225));
+
+                    SDL_UpdateWindowSurface(window);
+                } while (!exit);
+            }
+
+            SDL_DestroyWindow(window);
+        }
+
+        SDL_Quit();
+    }
+
     return 0;
 }
