1fbf13b8f479e5e7fa3b263e2522e073f50981ea
informaticaesne <informatica@esne.es>
2022-01-17 18:14:52 +0100

    Kernel and types added

--- headers/types.h
+++ headers/types.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef TYPES_H
+#define TYPES_H
+
+
+#include <iostream>
+namespace blood_engine {
+
+
+	typedef std::int_fast32_t i32;
+	typedef std::uint_fast32_t ui32;
+	typedef std::int_fast64_t i64;
+	typedef std::uint_fast64_t ui64;
+
+}
+
+
+#endif // !TYPES_H
---
+++ source/Kernel.cpp
@@ -0,0 +1,6 @@
+#include "..\headers\Kernel.h"
+
+void blood_engine::Kernel::Run()
+{
+	pool.start
+}
